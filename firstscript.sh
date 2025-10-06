42913e14803dc97b6303721f5edd55fa2ff5acef
rustynail-hack <mo.razakhan45@gmail.com>
2025-10-06 08:53:33 +0530

    Update firstscript.sh

--- firstscript.sh
+++ firstscript.sh
@@ -3,3 +3,18 @@
 
 echo "Hello World"
 
+#use of variable
+
+myname=Rustynail
+currentdate=$(date)
+currentdirectory=$(pwd)
+file=$(ls)
+
+echo "Hi,My name is $myname"
+ 
+echo "Today's date and time is $currentdate"
+
+echo "and i am working in this directory $currentdirectory"
+
+echo "and the files in the directory are $file"
+
